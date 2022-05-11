c38ab952d3b7dfbc47d2f180df339c15b2357656
Laerte <laerteakira@gmail.com>
2022-05-10 22:09:25 -0300

    POST /signup

--- controllers/sessionControllers.js
+++ controllers/sessionControllers.js
@@ -1,4 +1,5 @@
-// import 
+import joi from 'joi'; 
+import bcrypt from 'bcrypt';
 
 import db from "./../db.js";
 const sessionsCollection = db.collection("sessionsCollection");
@@ -12,10 +13,31 @@ export async function signIn (req, res) {
 };
 
 export async function signUp (req, res) {
-    try{
+    const schemaSignUp = joi.object({
+        name: joi.string().trim().required(),
+        email: joi.string().email().trim().required(),
+        password: joi.string().trim().required(),
+        confirmPassword: joi.string().required().valid(joi.ref('password'))
+    }) 
 
+    const validation = schemaSignUp.validate(req.body, {abortEarly:false});
+    if(validation.error){
+        console.log(validation.error.details.map((e) => e.message));
+        return res.status(422).send("Dados Inválidos");
+    } 
+
+    const {name, email, password} = req.body;
+    try{
+        const existeUsuario = await db.collection('usersCollection').findOne({email});
+        if(existeUsuario) return res.status(409).send('Usuário já existe!');
+
+        const passwordHash = bcrypt.hashSync(password, 10);
+
+        await db.collection('usersCollection').insertOne({name, email, password: passwordHash});
+
+        res.sendStatus(201);
     } catch (err){
-        res.send(err)
+        res.send(err);
     };
 };
 
